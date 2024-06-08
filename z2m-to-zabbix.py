79d6ede5e23576ef96a4e0d42e0e83f7472b5df4
enclude <enclude@users.noreply.github.com>
2024-06-08 21:07:13 +0200

    Create z2m-to-zabbix.py

---
+++ z2m-to-zabbix.py
@@ -0,0 +1,70 @@
+import json
+import os
+
+from paho.mqtt import client as mqtt_client
+broker = os.environ['MQTT_broker']
+port = 1883
+topic = os.environ['MQTT_topic']
+client_id = os.environ['MQTT_clientid']
+
+from zabbix_utils import Sender,ZabbixAPI,ItemValue
+sender = Sender(os.environ['ZABBIX_address'],10051)
+
+api = ZabbixAPI(os.environ['ZABBIX_address'],10051)
+api.login(token=os.environ['ZABBIX_token'])
+zabbixhosts = api.host.get()
+temp = ' '.join(str(x) for x in zabbixhosts)
+zabbixhosts = temp
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        # print("========================")
+        mqtttopic = msg.topic
+        mqtttopic = mqtttopic.lstrip("zigbee2mqtt/")
+        mqttpayload = msg.payload.decode()
+        mqttpayload = mqttpayload.replace("\\","")
+        # print("=")
+
+        if mqtttopic.find("logging") < 0:
+            if mqtttopic.find("ridge/") < 0:
+                # if mqtttopic.find("/set") < 0:
+                    # if mqtttopic.find("GROUND_TOILET_OCCUPANCY") < 0:
+                        # print(f"{mqttpayload} from {mqtttopic}")
+                if mqtttopic.find("availability") < 0:
+
+                    if mqtttopic in zabbixhosts:
+                        data = json.loads(mqttpayload)
+                        items = []
+                        print("--------------")
+                        print(mqtttopic)
+
+                        for key, value in data.items():
+                            print(key,value)
+                            item = ItemValue(mqtttopic, key, value)
+                            items.append(item)
+                        # print(items)
+                        response = sender.send(items)
+                        # print(response)
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+if __name__ == '__main__':
+    run()
